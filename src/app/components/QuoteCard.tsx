e04c74c423244ea07cfdb9628fbdeebb440a0318
yeonggyu-lim <yeonggyu.lim@navercorp.com>
2024-05-15 21:26:46 +0900

    feat: 명언 카드 컴포넌트 추가

---
+++ src/app/components/QuoteCard.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+interface QuoteCardProps {
+  author: string;
+  authorProfile: string;
+  quote: string;
+}
+
+const QuoteCard: React.FC<QuoteCardProps> = ({
+  author,
+  authorProfile,
+  quote,
+}) => {
+  return (
+    <div className="p-6">
+      <p className="text-lg font-medium mb-4 text-center">{quote}</p>
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-center">
+          <p className="font-semibold">{`- ${author} -`}</p>
+          <p className="text-sm">{authorProfile}</p>
+        </div>
+        <button className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded inline-block mt-4">
+          다른 명언 보기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteCard;
