142bfd475ed41f72fbd67acd81f23e00e72d2882
yeonggyu-lim <yeonggyu.lim@navercorp.com>
2024-05-16 00:05:01 +0900

    feat: google analytics 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-168VVRKCPB" />
     </html>
   );
 }
